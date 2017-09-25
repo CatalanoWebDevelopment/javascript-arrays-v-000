8a001411e756b6cd554cbcf74559ccd716caea5c
Parker Catalano <catalano.work@gmail.com>
2017-09-25 23:44:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [1];
   var element = 'foo';
   array.unshift(`${element}`)
   return array;
