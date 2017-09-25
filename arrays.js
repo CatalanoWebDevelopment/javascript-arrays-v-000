0bf564c9ced3b4ec08b94dfe4ab70ebbc9c8f85a
Parker Catalano <catalano.work@gmail.com>
2017-09-25 23:20:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
+  var array = [1];
+  var element = 'foo';
   array = [`${element}`, ...array];
   return array;
 }
 
-addElementToBeginningOfArray(array, 'foo');
+
